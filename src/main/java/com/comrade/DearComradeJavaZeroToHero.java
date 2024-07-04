875f8e3d9ed632e4e9ba4955259ac5df2e396344
shiva508 <dasarishiva1@gmail.com>
2024-07-04 07:36:43 +0530

    first commit

---
+++ src/main/java/com/comrade/DearComradeJavaZeroToHero.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+/**
+ * Hello world!
+ *
+ */
+public class DearComradeJavaZeroToHero
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
